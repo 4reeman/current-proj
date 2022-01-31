95592bed81a0ef0fb01523c5f3d97b1b5bbda050
Mr.freeman <y.oleksiuk@dev-branch.com>
2022-01-31 19:19:42 +0200

    audio block was created trouble with js

---
+++ web/themes/custom/fifth_sub_bootstrap/js/custom/platform_patch.js
@@ -0,0 +1,11 @@
+//@todo problems:)
+// (function ($) {
+//   Drupal.behaviors.firstBehavior = {
+//     attach: function (context, settings) {
+//       setTimeout(function(){
+//         $(context).find('.eapps-widget-toolbar').once('rm-widget-settings').css('visibility', 'collapse');
+//         $(context).find('.Embed__Inner-sc-gmu91j-1 + a').once('rm-widget-advertisement').css('visibility', 'collapse');
+//       },800);
+//     }
+//   };
+// })(jQuery);
