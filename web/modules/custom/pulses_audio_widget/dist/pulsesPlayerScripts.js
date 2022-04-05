d31244a0de940dcc246ba753c62c7de50e259450
Mr.freeman <y.oleksiuk@dev-branch.com>
2022-04-05 12:23:47 +0300

    Todo custom player

--- web/modules/custom/pulses_audio_widget/dist/pulsesPlayerScripts.js
+++ web/modules/custom/pulses_audio_widget/dist/pulsesPlayerScripts.js
@@ -1,11 +1,9 @@
+// Todo custom player
 (function ($, Drupal, drupalSettings) {
   'use strict';
   Drupal.behaviors.users_feedback_BasicFeedbackForm = {
     attach: function (context, settings) {
-     $(context).find('audio').once('remove').click(function() {
-       $(this).audioPlayer;
-     });
-    }
+
+      }
   };
 })(jQuery, Drupal, drupalSettings);
-
