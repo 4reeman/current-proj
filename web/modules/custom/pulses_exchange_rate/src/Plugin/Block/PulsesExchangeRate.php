74bb7992f90ea415e3738a5a6c944f76297a05a6
Mr.freeman <y.oleksiuk@dev-branch.com>
2022-04-09 20:00:49 +0300

    (4552.dev) start creating block which will display data from api

---
+++ web/modules/custom/pulses_exchange_rate/src/Plugin/Block/PulsesExchangeRate.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Drupal\pulses_exchange_rate\Plugin\Block;
+
+use Drupal\Core\Block\Annotation\Block;
+use Drupal\Core\Block\BlockBase;
+
+/**
+ * Provides a 'Exchange Rates' block.
+ *
+ * @Block(
+ *   id = "exchange_rates",
+ *   admin_label = @Translation("Exchange Rates"),
+ * )
+ */
+class PulsesExchangeRate extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    // '@todo Implement build() method.'
+  }
+
+}
