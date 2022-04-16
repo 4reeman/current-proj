6244457fe71251366a8323b71412e4691c21060e
Mr.freeman <y.oleksiuk@dev-branch.com>
2022-04-16 13:06:09 +0300

    (4552.dev) Block which display currency was created

--- web/modules/custom/pulses_exchange_rate/src/Plugin/Block/PulsesExchangeRate.php
+++ web/modules/custom/pulses_exchange_rate/src/Plugin/Block/PulsesExchangeRate.php
@@ -4,6 +4,10 @@ namespace Drupal\pulses_exchange_rate\Plugin\Block;
 
 use Drupal\Core\Block\Annotation\Block;
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\pulses_exchange_rate\CurrencyDataProviderInterface;
+use Drupal\pulses_exchange_rate\Form\ExchangeApiKey;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides a 'Exchange Rates' block.
@@ -13,13 +17,69 @@ use Drupal\Core\Block\BlockBase;
  *   admin_label = @Translation("Exchange Rates"),
  * )
  */
-class PulsesExchangeRate extends BlockBase {
+class PulsesExchangeRate extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * Instance of CurrencyDataProvider.
+   *
+   * @var \Drupal\pulses_exchange_rate\CurrencyDataProvider
+   */
+  public $client;
+
+  /**
+   * Construct PulsesExchangeRate class.
+   *
+   * Get instance of Client class.
+   *
+   * @param array $configuration
+   *   Current plugin configuration.
+   * @param string $plugin_id
+   *   Current plugin id.
+   * @param mixed $plugin_definition
+   *   Current plugin definition.
+   * @param \Drupal\pulses_exchange_rate\CurrencyDataProviderInterface $currencyProvider
+   *   Instance of currencyProvider class.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, CurrencyDataProviderInterface $currencyProvider) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->client = $currencyProvider;
+  }
+
+  /**
+   * Injecting dependencies.
+   *
+   * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
+   *   {@inheritDoc}.
+   * @param array $configuration
+   *   Current plugin configuration.
+   * @param string $plugin_id
+   *   Current plugin id.
+   * @param mixed $plugin_definition
+   *   Current plugin definition.
+   *
+   * @return static
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('pulses_exchange_rate_service'),
+    );
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    // '@todo Implement build() method.'
+    $api_key = \Drupal::config(ExchangeApiKey::SETTINGS)->get('key');
+    if (!empty($api_key)) {
+      $this->client->getResponse('https://api.currencyapi.com/v3/latest?apikey=', $api_key, TRUE);
+    }
+    return [
+      '#theme' => 'bla',
+      '#currency' => $this->client->data,
+    ];
   }
 
 }
