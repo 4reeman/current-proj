a5ef19ccbf3419198b11265ea051d4fc4b20fa1e
Mr.freeman <y.oleksiuk@dev-branch.com>
2022-04-20 17:29:37 +0300

    (4621.dev) Caching for data provided by openweathermap api was enabled

--- web/modules/custom/pulses_weather/src/PulsesWeatherProvider.php
+++ web/modules/custom/pulses_weather/src/PulsesWeatherProvider.php
@@ -2,6 +2,9 @@
 
 namespace Drupal\pulses_weather;
 
+use Drupal\Component\Datetime\TimeInterface;
+use Drupal\Core\Cache\CacheBackendInterface;
+
 /**
  * Provide possibility to get weather information for user.
  */
@@ -22,28 +25,55 @@ class PulsesWeatherProvider implements PulsesWeatherProviderInterface {
   private $userInfo;
 
   /**
+   * Instance of CacheBackend.
+   *
+   * @var \Drupal\Core\Cache\CacheBackendInterface
+   */
+  private $cache;
+
+  /**
+   * Instance of Time.
+   *
+   * @var \Drupal\Component\Datetime\TimeInterface
+   */
+  private $dateTime;
+
+  /**
    * PulsesWeatherProvider constructor.
    *
    * @param \Drupal\pulses_weather\PulsesResponseValidationInterface $response_data
    *   PulsesResponseValidation instance.
    * @param \Drupal\pulses_weather\PulsesCityProviderInterface $user_data
    *   PulsesCityProvider instance.
+   * @param \Drupal\Core\Cache\CacheBackendInterface $cache
+   *   CacheBackend object.
+   * @param \Drupal\Component\Datetime\TimeInterface $date_time
+   *   TimeInterface object.
    */
-  public function __construct(PulsesResponseValidationInterface $response_data, PulsesCityProviderInterface $user_data) {
+  public function __construct(PulsesResponseValidationInterface $response_data, PulsesCityProviderInterface $user_data, CacheBackendInterface $cache, TimeInterface $date_time) {
     $this->response = $response_data;
     $this->userInfo = $user_data;
+    $this->cache = $cache;
+    $this->dateTime = $date_time;
   }
 
   /**
    * {@inheritDoc}
    */
   public function getWeatherInfo($api_key) {
+    if ($cache = $this->cache->get($api_key)) {
+      return $cache->data;
+    }
     $user_data = $this->userInfo->getData();
     $city = $user_data['city_name'];
     $language = $user_data['langcode'];
     $key_param = '&appid=';
     $url = 'https://api.openweathermap.org/data/2.5/weather?q=' . $city . '&lang=' . $language . $key_param . $api_key;
     $data = $this->response->getDecodedData($url);
+    if (!empty($data)) {
+      $this->cache
+        ->set($api_key, $data, $this->dateTime->getRequestTime() + (86400));
+    }
     return $data;
   }
 
