89c3efcc3a71bacb295f1e3920bf758709692c7b
Mr.freeman <y.oleksiuk@dev-branch.com>
2022-04-20 15:38:41 +0300

    (4621.dev) PulsesWeather with own teamplate was created

--- web/modules/custom/pulses_weather/src/Plugin/Block/PulsesWeather.php
+++ web/modules/custom/pulses_weather/src/Plugin/Block/PulsesWeather.php
@@ -4,29 +4,78 @@ namespace Drupal\pulses_weather\Plugin\Block;
 
 use Drupal\Core\Block\Annotation\Block;
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\pulses_weather\Form\WeatherSettingsForm;
+use Drupal\pulses_weather\PulsesWeatherProviderInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
+ * Provides a 'Weather Info' block.
+ *
  * @Block(
  *    id = "weather_info",
  *    admin_label = @Translation("Weather Info"),
  * )
  */
-class WeatherSettingsForm extends BlockBase implements ContainerFactoryPluginInterface {
+class PulsesWeather extends BlockBase implements ContainerFactoryPluginInterface {
 
   /**
-   * @inheritDoc
+   * PulsesWeatherProvider instance.
+   *
+   * @var \Drupal\pulses_weather\PulsesWeatherProviderInterface
    */
-  public function build() {
-    // TODO: Implement build() method.
+  private $weatherService;
+
+  /**
+   * ConfigFactory instance.
+   *
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
+   */
+  private $configFactory;
+
+  /**
+   * PulsesWeather constructor.
+   *
+   * @param array $configuration
+   *   Current plugin configuration.
+   * @param string $plugin_id
+   *   Current plugin id.
+   * @param mixed $plugin_definition
+   *   Current plugin definition.
+   * @param \Drupal\pulses_weather\PulsesWeatherProviderInterface $pulses_weather
+   *   PulsesWeatherProvider instance.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   ConfigFactory instance.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, PulsesWeatherProviderInterface $pulses_weather, ConfigFactoryInterface $config_factory) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->weatherService = $pulses_weather;
+    $this->configFactory = $config_factory;
   }
 
   /**
-   * @inheritDoc
+   * {@inheritdoc}
+   */
+  public function build() {
+    $key = $this->configFactory->get(WeatherSettingsForm::SETTINGS)->get('api_key');
+    return [
+      '#theme' => 'weather',
+      '#data' => $this->weatherService->getWeatherInfo($key),
+    ];
+  }
+
+  /**
+   * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    // TODO: Implement create() method.
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('pulses_weather_service'),
+      $container->get('config.factory'),
+    );
   }
 
 }
